6ca410efa35749d5897388882170c4256de96945
hapadba <halit.patan@gmail.com>
2022-06-02 16:08:57 +0300

    fix

---
+++ services-aggregator/post-service/src/main/java/com/youngadessi/app/post/service/entity/CommentEntity.java
@@ -0,0 +1,42 @@
+package com.youngadessi.app.post.service.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "comment")
+@Getter
+@Setter
+public class CommentEntity {
+    @Id
+    private Long id;
+
+    @Column(name = "comment_text")
+    private String commentText;
+
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "post_id")
+    private String postId;
+
+    @Column(name = "create_time")
+    private Date createTime;
+
+    @Column(name = "update_time")
+    private Date updateTime;
+
+    @Column(name = "status")
+    private boolean status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private PostEntity post;
+}
