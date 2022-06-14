8c6b73458acf1e3785a605d23f0e27e47c1e7d47
hapadba <halit.patan@gmail.com>
2022-06-14 14:57:46 +0300

    fix

--- services-aggregator/user-service/src/main/java/com/youngadessi/app/user/service/model/User.java
+++ services-aggregator/user-service/src/main/java/com/youngadessi/app/user/service/model/User.java
@@ -7,21 +7,37 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Index;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "tbl_user")
+@Table(name = "tbl_user",uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"username"}),
+        @UniqueConstraint(columnNames = {"email"})
+},indexes = {
+        @Index(columnList = "fullname")
+})
 @Getter
 @Setter
-public class UserEntity extends BaseEntity {
+public class User extends BaseEntity {
 
     @Id
     @Column(name = "id")
     private Long id;
 
+    @Column(name = "fullname")
+    private String fullname;
+
+    @NotNull
+    @NotBlank
     @Column(name = "username")
     private String username;
 
+    @NotNull
+    @NotBlank
     @Column(name = "password")
     private String password;
 
