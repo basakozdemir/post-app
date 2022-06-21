0ccc3bc017bb664e244347220e0a31296beedb1c
hapadba <halit.patan@gmail.com>
2022-06-21 16:44:21 +0300

    fix

--- services-aggregator/user-service/src/main/java/com/youngadessi/app/user/api/UserController.java
+++ services-aggregator/user-service/src/main/java/com/youngadessi/app/user/api/UserController.java
@@ -1,13 +1,15 @@
 package com.youngadessi.app.user.api;
 
 import com.youngadessi.app.user.dto.UserCreateDTO;
-import com.youngadessi.app.user.dto.UserDeleteDTO;
 import com.youngadessi.app.user.dto.UserUpdateDTO;
 import com.youngadessi.app.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,23 +21,22 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/create")
+    @PostMapping
     public ResponseEntity<String> createUser(@RequestBody  UserCreateDTO userCreateDTO){
         userService.createUser(userCreateDTO);
         return new ResponseEntity<>("User created successfuly", HttpStatus.OK);
     }
 
-
-    @PostMapping("/delete")
-    public ResponseEntity<String> deleteUser(@RequestBody UserDeleteDTO userDeleteDTO){
-        userService.deleteUser(userDeleteDTO);
-        return new ResponseEntity<>("User is deleted", HttpStatus.OK);
-    }
-
-    @PostMapping("/update")
+    @PutMapping
     public ResponseEntity<String> updateUser(@RequestBody UserUpdateDTO userUpdateDTO){
         userService.updateUser(userUpdateDTO);
-        return new ResponseEntity<>("User is updated", HttpStatus.OK);
+        return new ResponseEntity<>("User updated successfuly", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return new ResponseEntity<>("User deleted successfuly", HttpStatus.OK);
     }
 
 }
--- services-aggregator/user-service/src/main/java/com/youngadessi/app/user/service/UserService.java
+++ services-aggregator/user-service/src/main/java/com/youngadessi/app/user/service/UserService.java
@@ -1,12 +1,13 @@
 package com.youngadessi.app.user.service;
 
 import com.youngadessi.app.user.dto.UserCreateDTO;
-import com.youngadessi.app.user.dto.UserDeleteDTO;
 import com.youngadessi.app.user.dto.UserUpdateDTO;
 import com.youngadessi.app.user.entity.User;
 import com.youngadessi.app.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -26,23 +27,20 @@ public class UserService {
 
     }
 
-    public void deleteUser(UserDeleteDTO userDeleteDTO){
-        User user = new User();
-        user.setId(userDeleteDTO.getId());
-
-        userRepository.delete(user);
-    }
-
     public void updateUser(UserUpdateDTO userUpdateDTO){
-        User user = new User();
-        userRepository.findById(userUpdateDTO.getId());
+
+        Optional<User> optionalUser = userRepository.findById(userUpdateDTO.getId());
+
+        User user = optionalUser.orElseThrow(()->new RuntimeException("User object with id "+userUpdateDTO.getId()+" not found on database"));
 
         user.setPassword(userUpdateDTO.getPassword());
         user.setEmail(userUpdateDTO.getEmail());
         user.setFullname(userUpdateDTO.getFullname());
 
-
-
         userRepository.save(user);
+    }
+
+    public void deleteUser(Long userId){
+        userRepository.deleteById(userId);
     }
 }
